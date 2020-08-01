1a06bec0a1d3ccbeb16461a0ee305f845b3f9aa9
Lucas Rudek <lucas-rudek@hotmail.com>
2020-08-01 12:13:14 -0300

    fetch API

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./styles.css";
 
-export default function Weather() {
+export default async function Weather() {
+  let api =
+    "http://api.openweathermap.org/data/2.5/weather?q=Prudent%C3%B3polis&units=metric&appid=17b6590bec41785683bf963c68520f35";
+
+  let response = await fetch(api);
+  let json = response.json();
+  console.log(json);
+
   return (
     <div className="container">
       <div className="weatherApp">
