e03f5a906c8c6f6798ebd10155ab9c9095329d41
Lucas Rudek <lucas-rudek@hotmail.com>
2020-08-13 11:32:11 -0300

    date fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather.js";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>One Week Weather App</h1>
       <Weather />
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -12,23 +12,24 @@ export default function Weather() {
     async function apiResponse() {
       const res = await axios(api_weather);
       const newRes = res.data.daily.map((res) => ({
-        day: res.dt,
         min: res.temp.min.toString().slice(0, 2),
         max: res.temp.max.toString().slice(0, 2),
         icon: res.weather[0].icon,
-        description: res.weather[0].description
+        description: res.weather[0].description,
+        day: new Date(res.dt * 1000).toLocaleString("en-US", {
+          weekday: "long"
+        })
       }));
       setData(newRes);
     }
     apiResponse();
   }, []);
 
-  console.log(data);
-
   return (
     <div className="container">
       {data.map((res) => (
         <div className="weatherApp">
+          <p>{res.day}</p>
           <img
             key={res.icon}
             src={`http://openweathermap.org/img/wn/${res.icon}@2x.png`}
