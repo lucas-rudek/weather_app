a9fbd90f91e6e637eb142f448b3c2dc9dd76b8f5
Lucas Rudek <lucas-rudek@hotmail.com>
2020-08-13 17:16:16 -0300

    added react router

---
+++ src/Monday.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as useParams } from "react-router-dom";
+
+export default function Monday() {
+  return (
+    <div>
+      <h2>Monday</h2>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./styles.css";
 import axios from "axios";
+import Monday from "./Monday.js";
 
 let api_weather =
   "https://api.openweathermap.org/data/2.5/onecall?lat=-25.2142293&lon=-50.9824194&&appid=17b6590bec41785683bf963c68520f35&units=metric&lang=pt_br";
@@ -11,9 +13,15 @@ export default function Weather() {
   useEffect(() => {
     async function apiResponse() {
       const res = await axios(api_weather);
-      const newRes = res.data.daily.map((res) => ({
-        min: res.temp.min.toString().slice(0, 2),
-        max: res.temp.max.toString().slice(0, 2),
+      const newRes = res.data.daily.map((res, index) => ({
+        min:
+          res.temp.min < 10
+            ? res.temp.min.toString().slice(0, 1)
+            : res.temp.min.toString().slice(0, 2),
+        max:
+          res.temp.max < 10
+            ? res.temp.max.toString().slice(0, 1)
+            : res.temp.max.toString().slice(0, 2),
         icon: res.weather[0].icon,
         description: res.weather[0].description,
         day: new Date(res.dt * 1000).toLocaleString("en-US", {
@@ -26,23 +34,32 @@ export default function Weather() {
   }, []);
 
   return (
-    <div className="container">
-      {data.map((res) => (
-        <div className="weatherApp">
-          <p>{res.day}</p>
-          <img
-            key={res.icon}
-            src={`http://openweathermap.org/img/wn/${res.icon}@2x.png`}
-            alt=""
-          />
-          <span>
-            <p key={res.min} className="min">
-              {res.min}°
-            </p>
-            <p key={res.max}>{res.max}°</p>
-          </span>
-        </div>
-      ))}
+    <div key="container" className="container">
+      <Router>
+        {data.map((res, index) => (
+          <Link to={`/${res.day}`} style={{ textDecoration: "none" }}>
+            <div key="" className="weatherApp">
+              <p key={index}>{res.day}</p>
+              <img
+                key={res.icon}
+                src={`http://openweathermap.org/img/wn/${res.icon}@2x.png`}
+                alt=""
+              />
+              <span>
+                <p key={res.min}>{res.min}°</p>
+                <p className="max" key={res.max}>
+                  {res.max}°
+                </p>
+              </span>
+            </div>
+          </Link>
+        ))}
+        <Switch>
+          <Route path={`/Monday`}>
+            <Monday />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
