aefb6425733e5699dd469dfdc1aafc78ab051dae
Lucas Rudek <lucas-rudek@hotmail.com>
2020-08-19 17:30:07 -0300

    some changes.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+document.title = "One Week Weather App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
